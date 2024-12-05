f22400d207ec343b886bcd4529f1bf5ba5c1f77e
Rajveer <rajveer.developer@icloud.com>
2024-12-05 13:27:55 +0530

    AoC - Day 5

---
+++ Day5/Part1.rs
@@ -0,0 +1,157 @@
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::{stdout, BufWriter, Stdout, Write};
+
+// use std::mem::{swap, replace};
+// use std::cmp::{max, min, Ordering, Reverse};
+
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+use std::ops::{
+    Add, AddAssign, Bound::Excluded, Bound::Included, Bound::Unbounded, Div, DivAssign, Mul,
+    MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
+use std::path::Path;
+use std::usize;
+
+use regex::Regex;
+
+// use std::rc::Rc;
+// use std::cell::RefCell;
+
+// use itertools::Itertools;
+
+// const MOD: i64 = 1_000_000_007;
+// const MOD: i64 = 998_244_353;
+
+// const BIG_INF: i64 = 1_000_000_000_000_000_000;
+// const SMALL_INF: i32 = 1_000_000_000;
+
+// const MAX_LIM: i32 = 2_000_000;
+
+// const DIR4: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+// const DIR8: [(i32, i32); 8] = [
+//     (1, 0), (0, 1), (-1, 0), (0, -1),
+//     (1, 1), (1, -1), (-1, 1), (-1, -1)
+// ];
+
+const MAX_SIZE: usize = 100;
+
+fn solve(reader: &mut Reader, writer: &mut BufWriter<Stdout>) {
+    let path = Path::new("day5_input.txt");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(err) => panic!("couldn't open {}: {}", display, err),
+        Ok(file) => file,
+    };
+
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(err) => panic!("couldn't open {}: {}", display, err),
+        Ok(_) => (),
+    }
+
+    let mut page_orderings: Vec<HashSet<usize>> = vec![HashSet::new(); MAX_SIZE];
+    let mut page_orders: Vec<Vec<usize>> = Vec::new();
+
+    for line in input.lines() {
+        if line.contains("|") {
+            let pages: Vec<usize> = line.split('|').map(|s| s.parse().unwrap()).collect();
+            page_orderings[pages[0]].insert(pages[1]);
+        } else if line.contains(",") {
+            let page_order: Vec<usize> = line.split(',').map(|s| s.parse().unwrap()).collect();
+            page_orders.push(page_order);
+        }
+    }
+
+    let mut total: i64 = 0;
+    for page_order in page_orders {
+        let mut ok = true;
+        for (i, &p1) in page_order.iter().enumerate() {
+            for &p2 in page_order.iter().skip(i + 1) {
+                ok &= page_orderings[p1].contains(&p2);
+            }
+        }
+        if ok {
+            total += page_order[page_order.len() / 2] as i64;
+        }
+    }
+
+    writeln!(writer, "{}", total).ok();
+}
+
+#[allow(dead_code)]
+fn gcd<T>(a: T, b: T) -> T
+where
+    T: Default + Copy + PartialEq + PartialOrd + Rem<Output = T>,
+{
+    return if b == T::default() { a } else { gcd(b, a % b) };
+}
+
+#[allow(dead_code)]
+fn lcm<T>(a: T, b: T) -> T
+where
+    T: Default
+        + Copy
+        + PartialEq
+        + PartialOrd
+        + Rem<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>,
+{
+    (a * b) / gcd(a, b)
+}
+
+#[derive(Default)]
+struct Reader {
+    buffer: Vec<String>,
+}
+
+#[allow(dead_code)]
+impl Reader {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed to parse!");
+            }
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read!");
+
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+
+    fn next_chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+
+    fn next_lines(&self) -> Vec<String> {
+        let mut input: Vec<String> = Vec::new();
+        for line in io::stdin().lines() {
+            if line.as_ref().unwrap().len() == 0 {
+                break;
+            }
+            input.push(line.unwrap());
+        }
+        input
+    }
+}
+
+fn main() {
+    let mut reader = Reader::default();
+    let writer = &mut BufWriter::new(stdout());
+
+    let is_multi_test: bool = false;
+    let mut t: i32 = 1;
+
+    if is_multi_test {
+        t = reader.next();
+    }
+
+    for _i in 0..t {
+        // write!(writer, "Case #{}: ", _i + 1).ok();
+        solve(&mut reader, writer);
+    }
+}
