1c24588420683c472542ed8ba7784d4c1c88d5a9
Rajveer <rajveer.developer@icloud.com>
2024-12-08 14:06:05 +0530

    AoC - Day 8

---
+++ Day8/Part2.rs
@@ -0,0 +1,194 @@
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::{stdout, BufWriter, Stdout, Write};
+
+// use std::mem::{swap, replace};
+// use std::cmp::{max, min, Ordering, Reverse};
+
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+use std::ops::{
+    Add, AddAssign, Bound::Excluded, Bound::Included, Bound::Unbounded, Div, DivAssign, Mul,
+    MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
+use std::path::Path;
+use std::time::Instant;
+use std::usize;
+
+use regex::Regex;
+
+// use std::rc::Rc;
+// use std::cell::RefCell;
+
+// use itertools::Itertools;
+
+// const MOD: i64 = 1_000_000_007;
+// const MOD: i64 = 998_244_353;
+
+// const BIG_INF: i64 = 1_000_000_000_000_000_000;
+// const SMALL_INF: i32 = 1_000_000_000;
+
+// const MAX_LIM: i32 = 2_000_000;
+
+// const DIR4: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+// const DIR8: [(i32, i32); 8] = [
+//     (1, 0),
+//     (0, 1),
+//     (-1, 0),
+//     (0, -1),
+//     (1, 1),
+//     (1, -1),
+//     (-1, 1),
+//     (-1, -1),
+// ];
+
+fn solve(reader: &mut Reader, writer: &mut BufWriter<Stdout>) {
+    let path = Path::new("day8_input.txt");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(err) => panic!("couldn't open {}: {}", display, err),
+        Ok(file) => file,
+    };
+
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(err) => panic!("couldn't open {}: {}", display, err),
+        Ok(_) => (),
+    }
+
+    let mut map: Vec<Vec<char>> = Vec::new();
+    for line in input.lines() {
+        let line_chars: Vec<char> = line.chars().collect();
+        map.push(line_chars);
+    }
+
+    let mut antennas: Vec<Vec<(i32, i32)>> = vec![Vec::new(); 123];
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            if map[i][j] != '.' {
+                let frequency = (map[i][j] as u32) as usize;
+                antennas[frequency].push((i as i32, j as i32));
+            }
+        }
+    }
+
+    for frequency in antennas {
+        for (i, &(x1, y1)) in frequency.iter().enumerate() {
+            for &(x2, y2) in frequency.iter().skip(i + 1) {
+                for k in 1..(map.len() + map[0].len()) as i32 {
+                    let (dx, dy) = (k * (x2 - x1), k * (y2 - y1));
+                    let (x3, y3) = (x2 + dx, y2 + dy);
+                    let (x4, y4) = (x1 - dx, y1 - dy);
+
+                    let mut atleast_one = false;
+                    if x3 >= 0 && x3 < map.len() as i32 && y3 >= 0 && y3 < map[0].len() as i32 {
+                        map[x3 as usize][y3 as usize] = '#';
+                        atleast_one = true;
+                    }
+                    if x4 >= 0 && x4 < map.len() as i32 && y4 >= 0 && y4 < map[0].len() as i32 {
+                        map[x4 as usize][y4 as usize] = '#';
+                        atleast_one = true;
+                    }
+
+                    if !atleast_one {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    let mut total: i32 = 0;
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            if map[i][j] != '.' {
+                total += 1;
+            }
+        }
+    }
+
+    writeln!(writer, "{}", total).ok();
+}
+
+#[allow(dead_code)]
+fn gcd<T>(a: T, b: T) -> T
+where
+    T: Default + Copy + PartialEq + PartialOrd + Rem<Output = T>,
+{
+    return if b == T::default() { a } else { gcd(b, a % b) };
+}
+
+#[allow(dead_code)]
+fn lcm<T>(a: T, b: T) -> T
+where
+    T: Default
+        + Copy
+        + PartialEq
+        + PartialOrd
+        + Rem<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>,
+{
+    (a * b) / gcd(a, b)
+}
+
+#[derive(Default)]
+struct Reader {
+    buffer: Vec<String>,
+}
+
+#[allow(dead_code)]
+impl Reader {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed to parse!");
+            }
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read!");
+
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+
+    fn next_chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+
+    fn next_lines(&self) -> Vec<String> {
+        let mut input: Vec<String> = Vec::new();
+        for line in io::stdin().lines() {
+            if line.as_ref().unwrap().len() == 0 {
+                break;
+            }
+            input.push(line.unwrap());
+        }
+        input
+    }
+}
+
+fn main() {
+    // let start = Instant::now();
+
+    let mut reader = Reader::default();
+    let writer = &mut BufWriter::new(stdout());
+
+    let is_multi_test: bool = false;
+    let mut t: i32 = 1;
+
+    if is_multi_test {
+        t = reader.next();
+    }
+
+    for _i in 0..t {
+        // write!(writer, "Case #{}: ", _i + 1).ok();
+        solve(&mut reader, writer);
+    }
+
+    // let elapsed = start.elapsed();
+    // let seconds = elapsed.as_secs_f64();
+
+    // writeln!(writer, "Elapsed time: {} seconds", seconds).ok();
+}
