99e8b6081071fc367e0848c35c527d11d8ee5bf2
Rajveer <rajveer.developer@icloud.com>
2024-12-18 13:19:24 +0530

    AoC - Day 18

---
+++ Day18/Part2.rs
@@ -0,0 +1,196 @@
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::{stdout, BufWriter, Stdout, Write};
+
+use std::mem::{replace, swap};
+// use std::cmp::{max, min, Ordering, Reverse};
+
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+use std::ops::{
+    Add, AddAssign, Bound::Excluded, Bound::Included, Bound::Unbounded, Div, DivAssign, Mul,
+    MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
+use std::path::Path;
+use std::time::Instant;
+use std::usize;
+
+use regex::Regex;
+
+// use std::rc::Rc;
+// use std::cell::RefCell;
+
+// use itertools::Itertools;
+
+// const MOD: i64 = 1_000_000_007;
+// const MOD: i64 = 998_244_353;
+
+// const BIG_INF: i64 = 1_000_000_000_000_000_000;
+// const SMALL_INF: i32 = 1_000_000_000;
+
+// const MAX_LIM: i32 = 2_000_000;
+
+const DIR4: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+// const DIR8: [(i32, i32); 8] = [
+//     (1, 0),
+//     (0, 1),
+//     (-1, 0),
+//     (0, -1),
+//     (1, 1),
+//     (1, -1),
+//     (-1, 1),
+//     (-1, -1),
+// ];
+
+const GRID_SIZE: usize = 71;
+const MAX_BYTES: usize = 1024;
+
+fn solve(reader: &mut Reader, writer: &mut BufWriter<Stdout>) {
+    let path = Path::new("day18_input.txt");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(err) => panic!("couldn't open {}: {}", display, err),
+        Ok(file) => file,
+    };
+
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(err) => panic!("couldn't open {}: {}", display, err),
+        Ok(_) => (),
+    }
+
+    let mut grid: Vec<Vec<char>> = vec![vec!['.'; GRID_SIZE]; GRID_SIZE];
+
+    let mut extra_byte_cnt: usize = 0;
+    let mut extra_bytes: Vec<(usize, usize)> = Vec::new();
+
+    for line in input.lines() {
+        let position: Vec<usize> = line.split(",").map(|p| p.parse().unwrap()).collect();
+        let (x, y) = (position[1], position[0]);
+        if extra_byte_cnt < MAX_BYTES {
+            grid[x][y] = '#';
+            extra_byte_cnt += 1;
+        } else {
+            extra_bytes.push((position[1], position[0]));
+        }
+    }
+
+    for &extra_byte in extra_bytes.iter() {
+        grid[extra_byte.0][extra_byte.1] = '#';
+
+        let mut queue: VecDeque<(usize, usize, i64)> = VecDeque::new();
+        let mut visited: Vec<Vec<bool>> = vec![vec![false; GRID_SIZE]; GRID_SIZE];
+
+        queue.push_back((0, 0, 0));
+        visited[0][0] = true;
+
+        let mut did_find_path: bool = false;
+        while let Some((x, y, steps)) = queue.pop_front() {
+            if (x, y) == (GRID_SIZE - 1, GRID_SIZE - 1) {
+                did_find_path = true;
+                break;
+            }
+            for &(dx, dy) in DIR4.iter() {
+                let (nx, ny) = (x as i32 + dx, y as i32 + dy);
+                if nx >= 0
+                    && nx < GRID_SIZE as i32
+                    && ny >= 0
+                    && ny < GRID_SIZE as i32
+                    && grid[nx as usize][ny as usize] == '.'
+                    && !visited[nx as usize][ny as usize]
+                {
+                    queue.push_back((nx as usize, ny as usize, steps + 1));
+                    visited[nx as usize][ny as usize] = true;
+                }
+            }
+        }
+
+        if !did_find_path {
+            writeln!(writer, "{},{}", extra_byte.1, extra_byte.0).ok();
+            break;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn gcd<T>(a: T, b: T) -> T
+where
+    T: Default + Copy + PartialEq + PartialOrd + Rem<Output = T>,
+{
+    return if b == T::default() { a } else { gcd(b, a % b) };
+}
+
+#[allow(dead_code)]
+fn lcm<T>(a: T, b: T) -> T
+where
+    T: Default
+        + Copy
+        + PartialEq
+        + PartialOrd
+        + Rem<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>,
+{
+    (a * b) / gcd(a, b)
+}
+
+#[derive(Default)]
+struct Reader {
+    buffer: Vec<String>,
+}
+
+#[allow(dead_code)]
+impl Reader {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed to parse!");
+            }
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read!");
+
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+
+    fn next_chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+
+    fn next_lines(&self) -> Vec<String> {
+        let mut input: Vec<String> = Vec::new();
+        for line in io::stdin().lines() {
+            if line.as_ref().unwrap().len() == 0 {
+                break;
+            }
+            input.push(line.unwrap());
+        }
+        input
+    }
+}
+
+fn main() {
+    // let start = Instant::now();
+
+    let mut reader = Reader::default();
+    let writer = &mut BufWriter::new(stdout());
+
+    let is_multi_test: bool = false;
+    let mut t: i32 = 1;
+
+    if is_multi_test {
+        t = reader.next();
+    }
+
+    for _i in 0..t {
+        // write!(writer, "Case #{}: ", _i + 1).ok();
+        solve(&mut reader, writer);
+    }
+
+    // let elapsed = start.elapsed();
+    // let seconds = elapsed.as_secs_f64();
+
+    // writeln!(writer, "Elapsed time: {} seconds", seconds).ok();
+}
